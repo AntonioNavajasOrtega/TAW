472868aaead7e702225f69f90aa1a296890a20b1
juanjose <juanjosetp02@uma.es>
2023-04-21 16:30:38 +0200

    ultimo caso de uso de asistente implementado

--- src/main/java/es/taw/sampletaw/dao/MensajeRepository.java
+++ src/main/java/es/taw/sampletaw/dao/MensajeRepository.java
@@ -1,7 +1,16 @@
 package es.taw.sampletaw.dao;
 
+import es.taw.sampletaw.entity.Cliente;
 import es.taw.sampletaw.entity.Mensaje;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MensajeRepository extends JpaRepository<Mensaje, Integer> {
+
+    @Query("select m from Mensaje m where m.clienteByEmisorCliente =: usuario or m.clienteByReceptorCliente = :usuario")
+
+    List<Mensaje> mensajesCuyoUsuarioEsEmisorOReceptor(@Param("usuario") Cliente usuario);
 }
